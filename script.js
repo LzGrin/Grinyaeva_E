b8131cc78728fa6a339e3638e4d6ae597ed15159
LzGrin <73195321+LzGrin@users.noreply.github.com>
2020-11-08 19:01:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var button = document.querySelector('.box');
 var counter = 0;
 var itemsList = document.getElementById('itemsList');
-var popup = document.getElementById('popup');
-var close = document.getElementById('close');
+var popup = document.getElementById('.popup');
+var close = document.getElementById('.close');
 var ids = [];
 var items = [];
 var load = document.getElementById('.loader_inner');
@@ -10,14 +10,13 @@ var loader = document.getElementById('.loader');
 
 async function changeCart(product, price, id) {
   /*document.querySelector('.box').style.background = "red";*/
-  document.querySelector('.box').addEventListener('click' => {
-  if (document.getElementById(".loader")) {
-    document.getElementById('.loader_inner').style.display != "block") {
-      document.getElementById('.loader_inner').style.display != "block", delay(5000);
+  if (document.querySelector('.box').addEventListener('click', openPopup, itemsList))  {
+  /*if (document.getElementById(".loader")) {*/
+    if (document.getElementById('.loader_inner').style.display = "block") {
+      document.getElementById('.loader_inner').style.display = "block", delay(5000);
     }
     else document.getElementById('.loader_inner').style.display = "none";
-  
-  });
+    };
 }
     counter++;
     document.getElementById('basket').innerText = counter;
@@ -33,40 +32,10 @@ async function changeCart(product, price, id) {
     itemsIds += items[i].title;
   }
 
+  document.getElementById('.cart').addEventListener('click', openPopup, popup); {
+  };
+    function openPopup() {
+    document.getElementById('.popup').style.visibility = 'visible';
+    document.getElementById('.close').style.visibility = 'hidden';
+    }
   
-
-  function openPopup() {
-    document.getElementById('popup').style.visibility = 'visible';
-    document.getElementById('close').style.visibility = 'hidden';
-  };
-}
-  console.log(changeCart)
-
-
-
-/*var loading = document.getElementById('cart');
-
-  document.getElementById('cart').addEventListener('click', l => {
-    l.
-  });*/
-
-/*$(button).load (function() {
-  document.querySelector('.box').addEventListener('click') ; {
-  $(".loader_inner").fadeOut();
-  $(".loader").delay(400).fadeOut("slow");
-  }
-});*/
- 
-/*async function loader() {
-  if (button = document.querySelector('.box').addEventListener('click')) {
-    document.getElementById(".loader_inner").delay(5000).fadeOut("slow");
-  } else {
-    document.getElementById(".loader_inner").fadeOut();
-  };
-}
-
-setTimeout(function onTimeout () {
-  document.getElementById(".loader_inner").fadeOut();
-  document.getElementById(".loader").delay(5000).fadeOut("slow");
-}, 5000);
-};*/
