f7b66208fb965d9bfb287f63ab751cc8cc7944a9
LzGrin <73195321+LzGrin@users.noreply.github.com>
2020-11-09 15:16:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-var button = document.querySelector('.box');
+/*var button = document.querySelector('.box');
 var counter = 0;
 var itemsList = document.getElementById('itemsList');
-var popup = document.getElementById('.popup');
-var close = document.getElementById('.close');
+var popup = document.getElementById('popup');
+var close = document.getElementById('close');
 var ids = [];
 var items = [];
-var load = document.getElementById('.loader_inner');
-var loader = document.getElementById('.loader');
+var load = document.getElementById('loader_inner');
+var loader = document.getElementById('loader');
 
 async function changeCart(product, price, id) {
-  /*document.querySelector('.box').style.background = "red";*/
-  if (document.querySelector('.box').addEventListener('click', openPopup, itemsList))  {
-  /*if (document.getElementById(".loader")) {*/
-    if (document.getElementById('.loader_inner').style.display = "block") {
-      document.getElementById('.loader_inner').style.display = "block", delay(5000);
-    }
-    else document.getElementById('.loader_inner').style.display = "none";
-    };
+  document.querySelector('.box').style.background = "red";
+  document.querySelector('box').addEventListener('click', openPopup, loader_inner);{
+  	document.getElementById('loader_inner').style.display = "block"; 
+  	setTimeout("Counter()", 5000);
+  	document.getElementById('basket').addEventListener('click', openPopup, popup);
+ }
 }
+
+  /*if (document.getElementById(".loader")) {*/
+    /*if (document.getElementById('loader_inner').style.display = "block") {
+      document.getElementById('loader_inner').style.display = "block", delay(5000);
+    }
+    else document.getElementById('loader_inner').style.display = "none";
+    };
+function Counter() {
+	document.getElementById('loader_inner').style.display = "none"; 
     counter++;
     document.getElementById('basket').innerText = counter;
     items.push({
@@ -32,10 +39,64 @@ async function changeCart(product, price, id) {
     itemsIds += items[i].title;
   }
 
-  document.getElementById('.cart').addEventListener('click', openPopup, popup); {
+}
+
+ /* document.getElementById('basket').addEventListener('click', openPopup, popup); {
   };
     function openPopup() {
-    document.getElementById('.popup').style.visibility = 'visible';
-    document.getElementById('.close').style.visibility = 'hidden';
+    document.getElementById('popup').style.visibility = 'visible';
+    document.getElementById('close').style.visibility = 'hidden';
+    }*/
+
+    var counter = 0;
+    var products = {};
+
+    function changeCart(title, price, id) {
+    	var basketElement = document.getElementById('basket');
+    	counter++;
+    	basketElement.innerText = counter;
+    	var product = products[id];
+    	if (product) {
+    		product.counter++;
+    	}else {
+    		products[id] = {
+    			title: title,
+    			price: price,
+    			id: id,
+    			count: 1
+    		};
+    	}
+
+    	updatePopupData();
+    	showLoader(function() {
+    		hideLoader();
+    		openPopup();
+    	});
+    	}
+    	function updatePopupData() {
+    		let itemsList = document.getElementById('itemsList');
+    		}
+    		function openPopup() {
+    			var popup = document.getElementById('popup');
+    			popup.style.visibility = 'visible';
+    		  	}
+
+    function closePopup() {
+    	var popup - document.getElementById('popup');
+    	popup.style.visibility = 'hidden';
     }
-  
+
+    function showLoader(callback) {
+    	var loader = document.getElementById('loader_inner');
+    	loader.style.visibility = 'visible';
+    	setTimeout(callback, 5000);
+    }
+
+    function hideLoader() {
+    	var loader = document.getElementById('loader');
+    	loader.style.visibility = 'hidden';
+    }
+
+    function Test() {
+    	alert("работает");
+    }
