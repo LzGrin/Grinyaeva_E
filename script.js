caf3088d750898e3dfb04c28cf13b96152887b5f
LzGrin <73195321+LzGrin@users.noreply.github.com>
2020-11-12 17:25:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,15 +16,86 @@ function changeCart(title, price, id) {
       count: 1
     };
   }
-
-  /*updatePopupData();*/
-
-  showLoader(function () {
-    hideLoader();
-    openPopup();
-  });
 }
-function updatePopupData() {
+
+var itemProduct = document.querySelectorAll('.product'),
+  cartCont = document.getElementById('cart_content');
+
+function addEventListener(elem, type, handler) {
+  if (elem.addEventListener) {
+    elem.addEventListener(type, handler, false);
+  } else {
+    elem.attachEvent('on' + type, function() {
+      handler.call(elem);
+    });
+  }
+  return false;
+}
+
+function getCartData() {
+  return JSON.parse(localStorage.getItem('popup'));
+}
+
+function setCartData(o) {
+  localStorage.setItem('popup', JSON.stringify(o));
+  return false;
+}
+
+function addToCart(e) {
+  this.disable = true;
+  var cartData = getCartData() || {},
+    parentProduct = this.parentNode,
+    itemId = this.getAttribute('data-id'),
+    itemTitle = parentProduct.querySelector('.product-title').innerHTML,
+    itemPrice = parentProduct.querySelector('.product-price').innerHTML;
+  if (cartData.hasOwnProperty(itemId)) {
+    cartData[itemId][2] += 1;
+  } else {
+    cartData[itemId] = [itemTitle, itemPrice, 1];
+  }
+  if (!setCartData(cartData)) {
+    this.disabled = false;
+  }
+  return false;
+}
+
+for (var i = 0; i < itemProduct.length; i++) {
+  addEvent(itemProduct[i].querySelector('.add_item'), 'click', addToCart);
+}
+
+function openPopup(e) {
+  var cartData = getCartData(),
+    totalItems = '';
+  if (cartData !== null) {
+    totalItems = '<table class="shopping_list"><tr><th>Наименование</th><th>Цена</th><th>Количество</th></tr>';
+    for (var items in cartData) {
+      totalItems += '<tr>';
+      for (var i = 0; i < cartData[items].length; i++) {
+        totalItems += '<td>' + cartData[items][i] + '</td>';
+      }
+      totalItems += '</tr>';
+    }
+    totalItems += '</table>';
+    cartCont.innerHTML = totalItems;
+  } else {
+    cartCont.innerHTML = 'Корзина пуста!';
+  }
+  return false;
+}
+
+addEventListener(document.getElementById('checkout'), 'click', changeCart);
+addEventListener(document.getElementById('clear_cart'), 'click', function(e){
+  localStorage.removeItem('.popup-list');
+  cartCont.innerHTML = 'Корзина очищена.';
+});
+
+
+showLoader(function () {
+  hideLoader();
+  openPopup();
+});
+
+/*function updatePopupData() {
   let itemsList = document.getElementById('itemsList');
 
   itemsList.innerHTML = '';
@@ -33,12 +104,12 @@ function updatePopupData() {
     let product = products[id];
     let basketItem = document.createElement("itemsList");
     basketItem.classList.add('basket-item');
-    basketItem.innerText = items[i].product-title;
-    basketItem.innerText = items[i].price;
+    basketItem.innerText = items[i].product - title;
+    basketItem.innerText = items[i].product-price;
 
     element.appendChild(itemsList);
   }
-}
+}*/
 function openPopup() {
   let popup = document.getElementById('popup');
   popup.style.visibility = 'visible';
