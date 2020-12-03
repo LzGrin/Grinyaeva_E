5203b9f1644178464aa95bcd5693c3a377f41880
LzGrin <73195321+LzGrin@users.noreply.github.com>
2020-12-03 16:38:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const countElement = document.getElementById('wrapper');
 
 const appElement = document.getElementById('app');
 const rows = [];
+const newPopup = new Popup();
+const popup = document.getElementById('popup');
+const loader = document.getElementById('loader');
 
 class form {
   constructor(wrapper) {
@@ -22,52 +25,73 @@ class form {
 
 class total {
 
-  constructor() {
-    const template = document.getElementById("total");
-    const content = document.importNode(template.content, true);
-    this.firstNameText = content.querySelector(".product-line");
-    const fornWrapper = content.querySelector(".jsRowForm");
-    this.form = new form(formWrapper);
-    this.form.onNameChange = (name) => {
-      this.firstNameText.innerText = name;
-      this.onRowChange(name);
-    };
-
-    wrapperElement.appendChaild(content);
-  }
-  set firstName(name) {
-    this.firstNameText.innerText = name;
-    this.form.firstName = name;
+  set firstName(price) {
+    this.firstNameText.innerText = price;
+    this.form.firstName = price;
   }
 
   get firstName() {
     return this.firstNameText.innerText;
   }
-}
 
-creatRow = (newName) => {
+  constructor() {
+    const template = document.getElementById("total");
+    const content = document.importNode(template.content, true);
+    this.firstNameText = content.querySelector(".product-line");
+    
+    this.valueInput = content.querySelector('.jsValue');
+    this.decreaseButton = content.querySelector('.jsDecrease');
+    this.increaseButton = content.querySelector('.jsIncrease');
+
+    this.valueInput.onblur = event => this.value = event.target.value;
+    this.increaseButton.onclick = () => this.increase();
+    this.decreaseButton.onclick = () => this.decrease();
+
+    wrapperElement.appendChaild(content);
+  }
+
+  increase() {
+    this.value++;
+  }
+
+  decrease() {
+    this.value--;
+  }
+}
+    let formWrapper = content.querySelector(".jsRowForm");
+    this.form = new form(formWrapper);
+    this.form.onNameChange = (price) => {
+    this.firstNameText.innerText = price;
+    this.onRowChange(price);
+    };
+
+    
+  
+
+ 
+
+creatRow = (newPrice) => {
   const newRow = new rows();
-  newRow.onRowChange = (name) => {
+  newRow.onRowChange = (price) => {
     appElement.innerHTML = `
-    <div>${name}</div>
+    <div>${price}</div>
     <div>
     ${rows.map((total) => total.firstName)}
     </div>
     `;
   };
-  newRowFirstName = newName;
+  newRowFirstPrice = newPrice;
   rows.push(newRow);
 };
 
-class popup {
+class Popup {
 
   constructor() {
     let counter = 0;
     const itemsList = document.getElementById('itemsList');
     let items = new Map();
     const basketElement = document.getElementById('basket-counter');
-    const popup = document.getElementById('popup');
-    const loader = document.getElementById('loader');
+   
 
     let changeCart = (title, price, articul) => {
       counter++;
@@ -81,6 +105,7 @@ class popup {
 
     }
     changeCart(title, price, articul);
+  }
 
     let openPopup = () => {
       loader.style.visibility = 'visible';
@@ -108,6 +133,9 @@ class popup {
 
       openPopup();
     }
+    
+    let newPopup = new Popup(title, price, articul);
+  
   
     
     let closePopup = () => {
@@ -115,107 +143,7 @@ class popup {
     }
     closePopup();
 
-  }
 
+
+class info {
 }
-
-
-
-    /*function changeCart(title, price, articul) {
-      counter++;
-      basketElement.innerText = counter;
-    
-     if (items.has(articul)) {
-      items.get(articul).count++;
-     } else {
-      items.set(articul, {title, price, "count":1});
-     } 
-      
-    }*/
-
-
-
-    /*function openPopup() {
-      loader.style.visibility = 'visible';
-      setTimeout(() => {
-        let templete = ``;
-        items.forEach(({title, price, count} ) => {
-          const {title, price} = element
-          templete += `
-              <div class="product-line">
-              <div class="product-line-item">${title}</div>
-               <div class="product-line-item">${price}</div>
-              <div class="product-line-item">${count}</div>
-            </div>
-           
-              
-              
-           `;
-         
-        
-         });
-        
-        itemsList.innerHTML = templete;
-        loader.style.visibility = 'hidden';
-        popup.style.visibility = 'visible';
-        
-        
-      }, 2000);
-      openPopup();
-    }
-  }
- 
-}*/
-
-    /*class count {
-
-    }
-
-
-    class info {
-
-    }*/
-
-
-
-
-
-
-
-
-
-    /*class Total {
-    
-      set value(val) {
-        this.valueInput.value = isNaN(val) ? 0 : val;
-      }
-    
-      get value() {
-        return +this.valueInput.value;
-      }
-    
-    constructor(wrapper) {
-      const total = document.getElementById('total');
-      const content = document.importNode(total.content, true);
-      
-      this.valueInput = content.querySelector('.jsValue');
-      this.decreaseButton = content.querySelector('.jsDecrease');
-      this.increaseButton = content.querySelector('.jsIncrease');
-    
-      this.valueInput.onblur = event => this.value = event.target.value;
-      this.increaseButton.onclick = () => this.increase();
-      this.decreaseButton.onclick = () => this.decrease();
-    
-      wrapper.appendChild(content);
-    }
-    
-    increase() {
-      this.value++;
-    }
-    
-    decrease() {
-      this.value--;
-    }
-    }
-    const appNode = document.querySelector('.app');
-    new Total(appNode);*/
