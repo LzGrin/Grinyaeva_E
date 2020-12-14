c4a7c8ce93b0332974f38ad73899064cf2f86e44
LzGrin <73195321+LzGrin@users.noreply.github.com>
2020-12-14 16:35:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,132 +1,129 @@
 const wrapperElement = document.getElementById("wrapper");
 let newPopup;
-const appNode = document.querySelector('.app');
-let counter;
+let counter = 0;
 let basketElement = document.getElementById('basket-counter');
 let items = new Map();
-const textCounter = document.getElementById("newCounter");
-
-
+​
 class Counter {
-
+​
   set value(val) {
     this.valueInput.value = isNaN(val) ? 0 : val;
   }
-
+​
   get value() {
     return +this.valueInput.value;
   }
-
+​
   constructor(wrapper) {
     const template = document.getElementById('counterTemplate');
     const content = document.importNode(template.content, true);
-
+​
     this.valueInput = content.querySelector('.jsValue');
     this.decreaseButton = content.querySelector('.jsDecrease');
     this.increaseButton = content.querySelector('.jsIncrease');
-
+​
     this.valueInput.onblur = (event) => {
       this.onCountChange(event.target.value);
       this.value = event.target.value;
     };
     this.increaseButton.onclick = () => this.increase();
     this.decreaseButton.onclick = () => this.decrease();
-
-
-
+​
     wrapper.appendChild(content);
   }
-
+​
   increase() {
     this.value++;
     this.onCountChange(this.value);
   }
-
+​
   decrease() {
     this.value--;
     this.onCountChange(this.value);
   }
-
+​
 }
-
-// new Counter(appNode);
-
-/*const counter = new Counter(appNode);
-counter.value = 25;
-counter.increase();*/
-
+​
 class Row {
-
+​
   constructor(row, product) {
+    this.product = product;
+​
     const template = document.getElementById('row');
     const content = document.importNode(template.content, true);
-    this.price = content.querySelector(".price");
-
-    // const counter = content.querySelector(".counter");
-    // this.newCounter = new Counter(counter);
-
-    const name = content.querySelector(".product");
-    name.innerText = product.title;
-
-    const price = content.querySelector(".price");
-    price.innerText = product.price;
- 
-    const priceWrapper = content.querySelector(".price");
-    this.price = new Counter(priceWrapper);
-    this.price.onCountChange = (price) => {
-      this.newRowPrice = price;
-      this.onRowChange(product);
+​
+    this.rowElement = content.querySelector('.row');
+​
+    const nameElement = content.querySelector(".name");
+    nameElement.innerText = product.title;
+​
+    this.priceElement = content.querySelector(".price");
+    this.priceElement.innerText = product.price;
+​
+    const counterElement = content.querySelector(".counter");
+    this.counter = new Counter(counterElement);
+    this.counter.value = product.count;
+    this.counter.onCountChange = (count) => {
+      this.priceElement.innerText = this.product.price * count;
+      this.product.count = count;
+      this.onRowChange(this.product);
     };
+​
+    this.deleteControl = content.querySelector(".delete");
+    this.deleteControl.onclick = () => this.delete();
+​
     row.appendChild(content);
   }
+​
+  delete() {
+    this.onRowDelete(this.product);
+    this.rowElement.innerHTML = "";
+  }
 }
-
-
+​
 class Popup {
-
+​
   constructor(popup) {
     let newPopupRow = document.getElementById("popup");
     const content = document.importNode(newPopupRow.content, true);
-
-    const newRow = content.querySelector(".popup-list");
-    // this.newRowPopup = new Row(newRow);
-
+​
+    const newRowElement = content.querySelector(".popup-list");
+​
     popup.appendChild(content);
-
+​
     items.forEach((product) => {
-      this.newRowPopup = new Row(newRow, product);
-      this.newRowPopup.onRowChange = (items) => {
-        this.newRowText = items;
-      }
+      this.newRow = new Row(newRowElement, product);
+      this.newRow.onRowChange = (product) => {
+        items.set(product.articul, product);
+      };
+      this.newRow.onRowDelete = (product) => {
+        items.delete(product.articul);
+        counter = counter - product.count;
+        basketElement.innerText = counter;
+      };
     });
-
-  
-   
   }
   close() {
     closePopup();
   }
 }
-
+​
 function openPopup() {
-
   newPopup = new Popup(wrapperElement);
-
 }
-
+​
 function closePopup() {
   newPopup = null;
   wrapperElement.innerHTML = "";
 }
-
+​
 let changeCart = (title, price, articul) => {
   counter++;
   basketElement.innerText = counter;
-
+​
   if (items.has(articul)) {
     items.get(articul).count++;
   } else {
-    items.set(articul, { title, price, "count": 1 });
+    items.set(articul, { title, price, "count": 1 , articul});
   }
-
-}
+};
